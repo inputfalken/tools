2fb7ff203b4493dc09711ad35315e534cc1038f1
Robert Andersson <robert.andersson.swe@gmail.com>
2019-07-20 21:25:31 +0200

    Initial commit

---
+++ TemplateFactory/CSharp.fs
@@ -0,0 +1,49 @@
+﻿namespace TemplateFactory
+open FSharp.Data
+open System
+
+module public CSharp =
+    type public Key = string
+    type public NameSpace = string
+    type public Value =
+                | Double of double
+                | Decimal of decimal
+                | String of string
+                | DateTime of DateTime
+                | Boolean of Boolean
+                | Array of Value seq
+                | Guid of Guid
+                | Null
+                | Object of Property seq
+    and public Property = Key * Value
+    type public File = {
+        NameSpace : NameSpace
+        Types : Value
+    }
+
+    let private stringParser =
+         function
+            | TryParse.Date x -> Value.DateTime x
+            | TryParse.Guid x -> Value.Guid x
+            | x -> Value.String x
+
+    // JSON is built on two structures:
+    // 1: A collection of name/value pairs
+    // 2: An ordered list of values.
+    let rec private map (value : JsonValue) =
+         match value with
+         | JsonValue.Number x -> Value.Decimal(x)
+         | JsonValue.Float x -> Value.Decimal(decimal x)
+         | JsonValue.String x -> stringParser x
+         | JsonValue.Boolean x -> Value.Boolean(x)
+         | JsonValue.Null -> Value.Null
+         | JsonValue.Array x -> x |> Seq.map (fun x -> map x) |> Value.Array
+         | JsonValue.Record x -> x |> Seq.map (fun x -> x ||> propertyMap) |> Value.Object
+    and private propertyMap (key : string) (value : JsonValue) = (key, map value)
+
+    let public ParseJson input =
+        let result = JsonValue.Parse input |> map
+        {
+            NameSpace = "TODO"
+            Types = result
+        }
---
+++ TemplateFactory/TryParse.fs
@@ -0,0 +1,21 @@
+namespace TemplateFactory
+module internal TryParse =
+    // convenient, functional TryParse wrappers returning option<'a>
+    let private tryParseWith (tryParseFunc : string -> (bool * _)) = tryParseFunc >> function
+        | true, v -> Some v
+        | false, _ -> None
+
+    let private parseDate = tryParseWith System.DateTime.TryParse
+    let private parseGuid = tryParseWith System.Guid.TryParse
+    let private parseInt = tryParseWith System.Int32.TryParse
+    let private parseSingle = tryParseWith System.Single.TryParse
+    let private parseDouble = tryParseWith System.Double.TryParse
+    // etc.
+
+    // active patterns for try-parsing strings
+    let (|Date|_|) = parseDate
+    let (|Guid|_|) = parseGuid
+    let (|Int|_|) = parseInt
+    let (|Single|_|) = parseSingle
+    let (|Double|_|) = parseDouble
+
