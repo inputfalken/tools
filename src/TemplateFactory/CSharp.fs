b4c50da30e2cb9d5b1d2d50674e301a8b406a013
Robert Andersson <robert.andersson.swe@gmail.com>
2019-07-21 15:25:56 +0200

    Use newlines to seperate types

--- src/TemplateFactory/CSharp.fs
+++ src/TemplateFactory/CSharp.fs
@@ -7,14 +7,15 @@ type CSharp =
     // With this syntax It's possible for CSharp to also call this function
     static member CreateFile(input : string,  [<Optional>] ?nameSpace) =
         let data = (input, nameSpace) ||> Json.parse
-        let rec stringifyObject property : string =
+        let rec stringifyObject (property : Property) (useNewline : bool) : string =
             let (key, value) = property
             let className = key + "Model"
             let stringifiedValue = stringifyValue value
+            let newLine = (if useNewline then Environment.NewLine else String.Empty)
 
             match value with
-            | Object x -> sprintf "public class %s { %s }" className stringifiedValue
-            | _ -> sprintf "public %s %s { get; set; }" stringifiedValue key
+            | Object x -> sprintf "%spublic class %s { %s }" newLine className stringifiedValue
+            | _ -> sprintf "%spublic %s %s { get; set; }" newLine stringifiedValue key
 
         and stringifyValue value : string =
             match value with
@@ -26,7 +27,7 @@ type CSharp =
             | Double x -> "double"
             | Null -> String.Empty
             | Array x -> x |> Seq.map stringifyValue |> Seq.reduce (fun x y -> x + y)
-            | Object x -> x |> Seq.fold (fun acc x -> acc + stringifyObject x) String.Empty
+            | Object x -> x |> Seq.mapi (fun x y -> (x, y)) |> Seq.fold (fun acc (index, x) -> acc + stringifyObject x (index <> 0)) String.Empty
 
         match nameSpace with
         | Some x -> sprintf "namespace %s {%s}" x (stringifyValue (data))
