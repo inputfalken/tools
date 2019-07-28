416ba630eb7df76400d3c490f2a59ec705f82435
Robert Andersson <robert.andersson.swe@gmail.com>
2019-07-28 18:43:11 +0200

    Fix assertion to looks correct for arrays

--- test/CSharpGeneratorTests/CSharpFileTests.fs
+++ test/CSharpGeneratorTests/CSharpFileTests.fs
@@ -2,8 +2,11 @@ module Tests
 open TemplateFactory
 open Xunit
 
-let assertion expected actual =
+let objectEntryAssertion expected actual =
     let expected = expected |> sprintf "public class RootModel { %s }"
+    Assert.Equal(expected, actual)
+    
+let arrayEntryAssertion expected actual =
     Assert.Equal(expected, actual)
 
 
@@ -11,43 +14,43 @@ let assertion expected actual =
 let Object() =
     let result = CSharp.CreateFile("""{"Foo": 2}""")
     let expected = "public decimal Foo { get; set; }"
-    assertion expected result
+    objectEntryAssertion expected result
 
 [<Fact>]
 let ``Empty Array``() =
     let result = CSharp.CreateFile("""[]""")
     let expected = "public object[] RootModel { get; set; }"
-    assertion expected result
+    arrayEntryAssertion expected result
     
 [<Fact>]
 let ``Array with single number``() =
     let result = CSharp.CreateFile("""[1]""")
     let expected = "public decimal[] RootModel { get; set; }"
-    assertion expected result
+    arrayEntryAssertion expected result
 
 [<Fact>]
 let ``Array with single string``() =
     let result = CSharp.CreateFile("""["bar"]""")
     let expected = "public string[] RootModel { get; set; }"
-    assertion expected result
+    arrayEntryAssertion expected result
 
 [<Fact>]
 let ``Array with numbers``() =
     let result = CSharp.CreateFile("""[1,2,3,4]""")
     let expected = "public decimal[] RootModel { get; set; }"
-    assertion expected result
+    arrayEntryAssertion expected result
 
 [<Fact>]
 let ``Array with strings``() =
     let result = CSharp.CreateFile("""["foo","bar"]""")
     let expected = "public string[] RootModel { get; set; }"
-    assertion expected result
+    arrayEntryAssertion expected result
 
 [<Fact>]
 let ``Array with strings and numbers``() =
     let result = CSharp.CreateFile("""[1,2,3,"foo"]""")
     let expected = "public object[] RootModel { get; set; }"
-    assertion expected result
+    arrayEntryAssertion expected result
     
 [<Fact>]
 let ``Array with objects``() =
@@ -61,8 +64,7 @@ let ``Array with objects``() =
 ]
 """
     let expected = "public class FooModel { public decimal Foo { get; set;} }"
-    assertion expected result
-
+    arrayEntryAssertion expected result
 
 [<Fact>]
 let ``Object with nested string``() =
@@ -76,25 +78,25 @@ let ``Object with nested string``() =
   }
 }""")
     let expected = "public class ApsModel { public string Alert { get; set; } }"
-    assertion expected result
+    objectEntryAssertion expected result
 
 [<Fact>]
 let ``Two Objects``() =
     let result = CSharp.CreateFile """{"foo": 2, "bar": "this is a test"}"""
     let expected = "public decimal Foo { get; set; } public string Bar { get; set; }"
-    assertion expected result
+    objectEntryAssertion expected result
 
 [<Fact>]
 let ``Object with nested int array``() =
     let result = CSharp.CreateFile """{"foo": 2, "items": [1,2,3,4,5]}"""
     let expected = "public decimal Foo { get; set; } public decimal[] Items { get; set; }"
-    assertion expected result
+    objectEntryAssertion expected result
     
 [<Fact>]
 let ``Object with nested string array``() =
     let result = CSharp.CreateFile """{"foo": 2, "items": ["foo","bar", "doe"]}"""
     let expected = "public decimal Foo { get; set; } public string[] Items { get; set; }"
-    assertion expected result
+    objectEntryAssertion expected result
     
     
 
