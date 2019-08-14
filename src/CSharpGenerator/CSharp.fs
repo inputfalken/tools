e6784bb5ba1cdff9a88bb6a6392f4cf73f36ec99
Robert Andersson <robert.andersson.swe@gmail.com>
2019-08-14 23:20:08 +0200

    Remove key argument from StringifyArray

--- src/CSharpGenerator/CSharp.fs
+++ src/CSharpGenerator/CSharp.fs
@@ -1,5 +1,7 @@
 ﻿namespace TemplateFactory
+open Formatters
 open JsonParser
+open System
 open System
 
 module private stringValidators =
@@ -7,12 +9,12 @@ module private stringValidators =
        input
        |> Option.Some
        |> Option.filter (fun x -> not (System.String.IsNullOrWhiteSpace(x)))
-       
+
 type CSharp =
-    static member CreateFile(input : string) =
+    static member CreateFile(input: string) =
         CSharp.CreateFile(input, new Settings())
 
-    static member CreateFile(input : string, settings : Settings) =
+    static member CreateFile(input: string, settings: Settings) =
         let data = (input, settings.Casing |> CasingRule.fromString |> Option.defaultValue CasingRule.Pascal) ||> Json.parse
         let classPrefix = settings.ClassPrefix
                           |> stringValidators.valueExists
@@ -25,8 +27,7 @@ type CSharp =
                          |> Option.defaultValue "Root"
                          |> (fun x -> sprintf "%s%s%s" classPrefix x classSuffix)
 
-
-        let rec stringifyValue value : string =
+        let rec stringifyValue value: string =
             match value with
             | DateTime x -> "System.DateTime"
             | Decimal x -> "decimal"
@@ -35,22 +36,19 @@ type CSharp =
             | Guid x -> "System.Guid"
             | Double x -> "double"
             | Null -> String.Empty
-            | Array x -> stringifyArray rootObject x
+            | Array x -> stringifyArray x
             | Object x -> x |> stringifyObject
 
-        and stringifyArray (key : string) (value : Value seq) =
-            if Seq.isEmpty value then Formatters.arrayProperty "object" key
+        and stringifyArray (value: Value seq): string =
+            if Seq.isEmpty value then "object"
             else
-                value
-                |> Seq.map (fun x ->
-                    match x with
-                    | Object x -> x |> stringifyObject |> (fun x -> Formatters.``class`` key x)
-                    | x -> x |> stringifyValue |> (fun x -> Formatters.arrayProperty x key)
-                )
-                |> Seq.reduce (fun x y -> if y = x then y else "object" |> (fun x -> Formatters.arrayProperty x key))
+                let result = value
+                            |> Seq.map stringifyValue
+                            |> Seq.reduce (fun x y -> if x = y then y else "object")
 
+                result
 
-        and stringifyObject (properties : Property seq) : string =
+        and stringifyObject (properties: Property seq): string =
             properties
             |> Seq.mapi (fun index property ->
                 let space = (if index <> 0 then " " else String.Empty)
@@ -60,11 +58,12 @@ type CSharp =
                     let stringifiedValue = stringifyObject x
                     Formatters.``class`` className stringifiedValue |> (fun x -> sprintf "%s%s" space x)
                 | Array x ->
-                    let stringifiedValue = stringifyArray property.Key x
+                    let stringifiedValue = stringifyArray x |> Formatters.arrayProperty <| property.Key
                     sprintf "%s%s" space stringifiedValue
                 | x ->
                     let stringifiedValue = stringifyValue x
-                    Formatters.property stringifiedValue property.Key |> (fun x -> sprintf "%s%s" space x)
+                    let formatted = Formatters.property stringifiedValue property.Key |> (fun x -> sprintf "%s%s" space x)
+                    formatted
             )
             |> Seq.reduce (fun acc curr -> acc + curr)
 
@@ -79,7 +78,12 @@ type CSharp =
             2: An ordered list of values.
         """
         match data with
-        | Array x -> x |> stringifyArray rootObject
-        | Object x -> x |> stringifyObject |> Formatters.``class`` rootObject
+        | Array x ->
+                     let ``type`` = stringifyArray x
+                     let property = ``type`` |> Formatters.arrayProperty <| rootObject
+                     property
+        | Object x ->
+            let content = x |> stringifyObject
+            Formatters.``class`` rootObject content
         | _ -> raise (new ArgumentException(error))
         |> namespaceFormatter
