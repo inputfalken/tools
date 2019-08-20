f35f5fa00a27ac49ecaa47c14d3bfd3c45a8e557
Robert Andersson <robert.andersson.swe@gmail.com>
2019-08-20 20:32:22 +0200

    Add buffer functionality for piping multiple lines

--- src/PowerShellModule/CSharpFactory.fs
+++ src/PowerShellModule/CSharpFactory.fs
@@ -1,4 +1,7 @@
 ﻿namespace PowerShellModule
+open System
+open System.Collections.Generic
+open System.Collections.Generic
 open System.Management.Automation
 open TemplateFactory
 
@@ -8,29 +11,32 @@ type CSharpFactory() =
 
     [<ValidateNotNullOrEmpty>]
     [<Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)>]
-    member val Input : string = "" with get, set
+    member val InputObject: string = "" with get, set
 
     [<ValidateSet("Camel", "Pascal")>]
     [<Parameter(Mandatory = false, Position = 1)>]
-    member val Casing : string = "Pascal" with get, set
+    member val Casing: string = "Pascal" with get, set
 
     [<ValidateNotNullOrEmpty>]
     [<Parameter(Mandatory = false, Position = 2 )>]
-    member val NameSpace : string = "" with get, set
+    member val NameSpace: string = "" with get, set
 
     [<ValidateNotNullOrEmpty>]
     [<Parameter(Mandatory = false, Position = 3 )>]
-    member val RootObjectName : string = "Root" with get, set
+    member val RootObjectName: string = "Root" with get, set
 
     [<ValidateNotNullOrEmpty>]
     [<Parameter(Mandatory = false, Position = 4 )>]
-    member val ClassPrefix : string = "" with get, set
+    member val ClassPrefix: string = "" with get, set
 
     [<ValidateNotNullOrEmpty>]
     [<Parameter(Mandatory = false, Position = 5 )>]
-    member val ClassSuffix : string = "" with get, set
+    member val ClassSuffix: string = "" with get, set
 
+    member val private Buffer: List<string> = new List<string>() with get
 
+    override x.ProcessRecord() = x.Buffer.Add(x.InputObject)
+    
     override x.EndProcessing() =
         let settings = new Settings(
                            Casing = x.Casing,
@@ -39,4 +45,8 @@ type CSharpFactory() =
                            ClassSuffix = x.ClassSuffix,
                            RootObjectName = x.RootObjectName
                        )
-        CSharp.CreateFile(x.Input, settings) |> x.WriteObject
+        // TODO add proper support like https://github.com/PowerShell/PowerShell/blob/master/src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/ConvertFromJsonCommand.cs
+        CSharp.CreateFile(System.String.Join(Environment.NewLine, x.Buffer), settings) |> x.WriteObject
+        
+        
+        
