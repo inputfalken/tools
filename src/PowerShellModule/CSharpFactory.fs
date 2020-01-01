91abe8c0c68e2c20a599af642a7b27ca10cd9608
Robert Andersson <robert.andersson.swe@gmail.com>
2020-01-01 22:11:05 +0100

    Add documenation and rename arguments of PowerShellModule

--- src/PowerShellModule/CSharpFactory.fs
+++ src/PowerShellModule/CSharpFactory.fs
@@ -1,4 +1,5 @@
 ﻿namespace PowerShellModule
+
 open System
 open System.Collections.Generic
 open System.Management.Automation
@@ -10,41 +11,35 @@ type CSharpFactory() =
     inherit PSCmdlet()
 
     [<ValidateNotNullOrEmpty>]
-    [<Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)>]
+    [<Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true, HelpMessage = "The JSON.")>]
     member val InputObject: string = "" with get, set
 
     [<ValidateSet("Camel", "Pascal")>]
-    [<Parameter(Mandatory = false, Position = 1)>]
+    [<Parameter(Mandatory = false, Position = 1, HelpMessage = "Sets the casing rules for class and property names.")>]
     member val Casing: string = "Pascal" with get, set
 
     [<ValidateNotNullOrEmpty>]
-    [<Parameter(Mandatory = false, Position = 2 )>]
+    [<Parameter(Mandatory = false, Position = 2, HelpMessage = "Sets the name of the namespace.")>]
     member val NameSpace: string = "" with get, set
 
     [<ValidateNotNullOrEmpty>]
-    [<Parameter(Mandatory = false, Position = 3 )>]
-    member val RootObjectName: string = "Root" with get, set
+    [<Parameter(Mandatory = false, Position = 3, HelpMessage = "Sets the name of the root object.")>]
+    member val ObjectName: string = "Root" with get, set
 
-    [<Parameter(Mandatory = false, Position = 4 )>]
-    member val ClassPrefix: string = "" with get, set
+    [<Parameter(Mandatory = false, Position = 4, HelpMessage = "Sets a prefix for the class names.")>]
+    member val Prefix: string = "" with get, set
 
-    [<Parameter(Mandatory = false, Position = 5 )>]
-    member val ClassSuffix: string = "" with get, set
+    [<Parameter(Mandatory = false, Position = 5, HelpMessage = "Suffix a prefix for the class names.")>]
+    member val Suffix: string = "" with get, set
 
-    member val private Buffer: List<string> = List<string>() with get
-    
+    member val private Buffer: List<string> = List<string>()
+
     override x.ProcessRecord() = x.Buffer.Add(x.InputObject)
-    
+
     override x.EndProcessing() =
-        let settings = Settings(
-                           Casing = x.Casing,
-                           NameSpace = x.NameSpace,
-                           ClassPrefix = x.ClassPrefix,
-                           ClassSuffix = x.ClassSuffix,
-                           RootObjectName = x.RootObjectName
-                       )
+        let settings =
+            Settings
+                (Casing = x.Casing, NameSpace = x.NameSpace, ClassPrefix = x.Prefix, ClassSuffix = x.Suffix,
+                 RootObjectName = x.ObjectName)
         // TODO add proper support like https://github.com/PowerShell/PowerShell/blob/master/src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/ConvertFromJsonCommand.cs
         CSharp.CreateFile(String.Join(Environment.NewLine, x.Buffer), settings) |> x.WriteObject
-        
-        
-        
