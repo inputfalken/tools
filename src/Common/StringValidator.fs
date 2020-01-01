45934c11079cd72db3637c3a29000a727f4bd55d
Robert Andersson <robert.andersson.swe@gmail.com>
2020-01-01 21:53:34 +0100

    Create util functions for combining strings

--- src/Common/StringValidator.fs
+++ src/Common/StringValidator.fs
@@ -1,7 +1,15 @@
-module public Common.StringValidator
+namespace Common
 
-let public valueExists input =
-    input
-    |> Option.Some
-    |> Option.filter (fun x -> not (System.String.IsNullOrWhiteSpace(x)))
-    |> Option.map (fun x -> x.Trim())
+module public StringValidator =
+    let public valueExists input =
+        input
+        |> Option.Some
+        |> Option.filter (fun x -> not (System.String.IsNullOrWhiteSpace(x)))
+        |> Option.map (fun x -> x.Trim())
+
+module public StringUtils =
+    let joinStrings strings: string =
+        strings |> String.concat System.String.Empty
+        
+    let joinStringsWithSpaceSeparation strings: string =
+        strings |> String.concat " "
