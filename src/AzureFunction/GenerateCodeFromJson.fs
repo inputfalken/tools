f068fad9042f908bd6b3d3a145a549e264dc3241
Robert Andersson <robert.andersson.swe@gmail.com>
2020-01-12 14:51:24 +0100

    Integrate CSharpGenerator

--- src/AzureFunction/GenerateCodeFromJson.fs
+++ src/AzureFunction/GenerateCodeFromJson.fs
@@ -12,39 +12,31 @@ open Microsoft.Extensions.Logging
 module GenerateCodeFromJsonFN =
     // Define a nullable container to deserialize into.
     [<AllowNullLiteral>]
-    type NameContainer() =
-        member val Name = "" with get, set
+    type JsonContainer() =
+        member val Json = String.Empty with get, set
 
     // For convenience, it's better to have a central place for the literal.
     [<Literal>]
     let Name = "name"
 
     [<FunctionName("GenerateCodeFromJsonFN")>]
-    let run ([<HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)>]req: HttpRequest) (log: ILogger) =
+    let run ([<HttpTrigger(AuthorizationLevel.Function, "post", Route = null)>] req: HttpRequest) (log: ILogger) =
         async {
             log.LogInformation("F# HTTP trigger function processed a request.")
-
-            let nameOpt =
-                if req.Query.ContainsKey(Name) then
-                    Some(req.Query.[Name].[0])
-                else
-                    None
 
             use stream = new StreamReader(req.Body)
             let! reqBody = stream.ReadToEndAsync() |> Async.AwaitTask
 
-            let data = JsonConvert.DeserializeObject<NameContainer>(reqBody)
+            let data = JsonConvert.DeserializeObject<JsonContainer>(reqBody)
+            
+            let json =
+                match data with
+                | null -> ""
+                | nc -> nc.Json
 
-            let name =
-                match nameOpt with
-                | Some n -> n
-                | None ->
-                   match data with
-                   | null -> ""
-                   | nc -> nc.Name
-
-            if not (String.IsNullOrWhiteSpace(name)) then
-                return OkObjectResult(sprintf "Hello, %s" name) :> IActionResult
+            if not (String.IsNullOrWhiteSpace(json)) then
+                return OkObjectResult(CSharpGenerator.CSharp.CreateFile(json)) :> IActionResult
             else
-                return BadRequestObjectResult("Please pass a name on the query string or in the request body") :> IActionResult
-        } |> Async.StartAsTask
+                return BadRequestObjectResult("Could not find json") :> IActionResult
+        }
+        |> Async.StartAsTask
