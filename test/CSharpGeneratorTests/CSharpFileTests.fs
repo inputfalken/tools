4ed24b6db462910406599771f5dbf40ecdb9df9f
Robert Andersson <robert.andersson.swe@gmail.com>
2020-03-01 21:37:32 +0100

    Group theory unit tests

--- test/CSharpGeneratorTests/CSharpFileTests.fs
+++ test/CSharpGeneratorTests/CSharpFileTests.fs
@@ -5,15 +5,27 @@ open CSharpGenerator.Arguments
 open Common.Casing
 open Xunit
 
-[<Fact>]
-let ``Passing valid JSON sets a value in Value``() =
-    let result = CSharp.CreateFile(""" { } """)
-    Assert.NotNull(result.Either.Value)
-    Assert.Null(result.Either.Error)
 
-[<Fact>]
-let ``Passing invalid JSON sets a value in Value``() =
-    let result = CSharp.CreateFile("""abc""")
+[<Theory>]
+[<InlineData("""{}""")>]
+let ``Empty object is valid`` json =
+    let result = CSharp.CreateFile json
+    let expected = "public object Root { get; set; }"
+    Assert.Equal(expected, result.Either.Value)
+
+[<Theory>]
+[<InlineData("""[]""")>]
+let ``Empty Array is valid`` json =
+    let result = CSharp.CreateFile json
+    let expected = "public object[] Root { get; set; }"
+    Assert.Equal(expected, result.Either.Value)
+
+[<Theory>]
+[<InlineData("""1""")>]
+[<InlineData("""abc""")>]
+[<InlineData(""" "abc" """)>]
+let ``Passing invalid JSON sets a value in Value`` json =
+    let result = CSharp.CreateFile json
     Assert.Null(result.Either.Value)
     Assert.NotNull(result.Either.Error)
 
@@ -186,13 +198,6 @@ let ``Two array Objects next to each other with different properties should expa
     Assert.Equal(expected, result.Either.Value)
 
 [<Theory>]
-[<InlineData("""{"Foo": 2}""")>]
-let Object json =
-    let result = CSharp.CreateFile json
-    let expected = "public class RootModel { public decimal Foo { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
 [<InlineData("""{ "Foo" : [] }""")>]
 let ``Object with Empty Array`` json =
     let result = CSharp.CreateFile json
@@ -208,213 +213,81 @@ let ``Object with Empty or null object`` json =
     Assert.Equal(expected, result.Either.Value)
 
 [<Theory>]
-[<InlineData("""{}""")>]
-let ``Empty object`` json =
+[<InlineData("""["2020-01-01", "3/1/2020 9:05:20 PM"]""", "System.DateTime")>]
+[<InlineData("""["2020-01-01"]""", "System.DateTime")>]
+[<InlineData("""["3/1/2020 9:05:20 PM"]""", "System.DateTime")>]
+[<InlineData("""["85a34f88-d688-46ce-9989-89d7a8698de7", "79607c8b-74b8-4293-8e01-ca2f77bf76a7"]""", "System.Guid")>]
+[<InlineData("""["85a34f88-d688-46ce-9989-89d7a8698de7"]""", "System.Guid")>]
+[<InlineData("""["foo", "bar"]""", "string")>]
+[<InlineData("""["foo"]""", "string")>]
+[<InlineData("""[1, 10]""", "decimal")>]
+[<InlineData("""[1]""", "decimal")>]
+[<InlineData("""[null, null]""", "object")>]
+[<InlineData("""[null, {}]""", "object")>]
+[<InlineData("""[null]""", "object")>]
+[<InlineData("""[{}, {}]""", "object")>]
+[<InlineData("""[{}]""", "object")>]
+let ``Array with basetypes`` json ``type`` =
     let result = CSharp.CreateFile json
-    let expected = "public object Root { get; set; }"
+    let expected = sprintf "public %s[] Root { get; set; }" ``type``
     Assert.Equal(expected, result.Either.Value)
 
 [<Theory>]
-[<InlineData("""[]""")>]
-let ``Empty Array`` json =
+[<InlineData("""["2020-01-01", "3/1/2020 9:05:20 PM", null]""", "System.DateTime?")>]
+[<InlineData("""["2020-01-01", null, "3/1/2020 9:05:20 PM"]""", "System.DateTime?")>]
+[<InlineData("""["2020-01-01", null]""", "System.DateTime?")>]
+[<InlineData("""["3/1/2020 9:05:20 PM", null]""", "System.DateTime?")>]
+[<InlineData("""["85a34f88-d688-46ce-9989-89d7a8698de7", "79607c8b-74b8-4293-8e01-ca2f77bf76a7", null]""", "System.Guid?")>]
+[<InlineData("""["85a34f88-d688-46ce-9989-89d7a8698de7", null,"79607c8b-74b8-4293-8e01-ca2f77bf76a7"]""", "System.Guid?")>]
+[<InlineData("""["85a34f88-d688-46ce-9989-89d7a8698de7", null]""", "System.Guid?")>]
+[<InlineData("""["foo", "bar", null]""", "string")>]
+[<InlineData("""["foo", null, "bar"]""", "string")>]
+[<InlineData("""["foo", null]""", "string")>]
+[<InlineData("""[1, 10, null]""", "decimal?")>]
+[<InlineData("""[1, null]""", "decimal?")>]
+[<InlineData("""[1,null, 10]""", "decimal?")>]
+[<InlineData("""[null ,"2020-01-01", "3/1/2020 9:05:20 PM"]""", "System.DateTime?")>]
+[<InlineData("""[null ,"85a34f88-d688-46ce-9989-89d7a8698de7", "79607c8b-74b8-4293-8e01-ca2f77bf76a7"]""", "System.Guid?")>]
+[<InlineData("""[null, {}, null]""", "object")>]
+[<InlineData("""[null,"foo", "bar"]""", "string")>]
+[<InlineData("""[null,1, 10]""", "decimal?")>]
+[<InlineData("""[{}, null, {}]""", "object")>]
+[<InlineData("""[{}, null]""", "object")>]
+[<InlineData("""[{}, {}, null]""", "object")>]
+let ``Array with basetypes mixed with null`` json ``type`` =
     let result = CSharp.CreateFile json
-    let expected = "public object[] Root { get; set; }"
+    let expected = sprintf "public %s[] Root { get; set; }" ``type``
     Assert.Equal(expected, result.Either.Value)
 
 [<Theory>]
-[<InlineData("""[1]""")>]
-let ``Array with single number`` json =
+[<InlineData("""[ { "foo": 1, "bar": "Hi" }, { "foo": 2, "bar": "Hi" } ] """, "public decimal Foo { get; set; } public string Bar { get; set; }")>]
+[<InlineData("""[ { "bar": [1, "foo"] }, { "bar": [1, 2] } ] """, "public object[] Bar { get; set; }")>]
+[<InlineData("""[ { "bar": [1, 2] }, { "bar": [1, "foo"] } ]""", "public object[] Bar { get; set; }")>]
+[<InlineData("""[ { "Foo" : "foobar", "John" : 2 }, { "Bar" : null, "Doe" : 2 } ]""", "public string Foo { get; set; } public decimal? John { get; set; } public object Bar { get; set; } public decimal? Doe { get; set; }")>]
+[<InlineData("""[ { "Foo" : null, "John" : null }, { "Bar" : null, "Doe" : null }, { "Foo" : "foo", "John" : "doo", "Bar" : 2, "Doe" : 4 } ]""", "public string Foo { get; set; } public string John { get; set; } public decimal? Bar { get; set; } public decimal? Doe { get; set; }")>]
+[<InlineData("""[ { "Foo" : "foobar", "John" : 2 }, { "Bar" : null, "Doe" : 2 }, { "Foo" : null, "John" : 2 }, { "Bar" : null, "Doe" : 2 } ]""", "public string Foo { get; set; } public decimal? John { get; set; } public object Bar { get; set; } public decimal? Doe { get; set; }")>]
+[<InlineData("""[ { "Foo" : null, "Bar" : 2 }, { "Foo" : 4, "Bar" : 2 } ]""", "public decimal? Foo { get; set; } public decimal Bar { get; set; }")>]
+[<InlineData("""[ { "Foo" : null, "Bar" : 2 }, { "Foo" : "foobar", "Bar" : 2 } ]""", "public string Foo { get; set; } public decimal Bar { get; set; }")>]
+[<InlineData("""[ { "Foo" : null, "Bar" : 2, "test" : 2 }, { "Foo" : "foobar", "Bar" : 2 } ]""", "public string Foo { get; set; } public decimal Bar { get; set; } public decimal? Test { get; set; }")>]
+[<InlineData("""[ { "Foo" : null, "Bar" : 2 }, { "Foo" : "foobar", "Bar" : 2, "test" : 2 } ]""", "public string Foo { get; set; } public decimal Bar { get; set; } public decimal? Test { get; set; }")>]
+let ``Array with objects`` json csharp =
     let result = CSharp.CreateFile json
-    let expected = "public decimal[] Root { get; set; }"
+    let expected = sprintf "public class RootModel { %s }" csharp
     Assert.Equal(expected, result.Either.Value)
 
 [<Theory>]
-[<InlineData("""["bar"]""")>]
-let ``Array with single string`` json =
+[<InlineData("""{ "aps": { "alert":"tit}" } }""", "public class ApsModel { public string Alert { get; set; } } public ApsModel Aps { get; set; }")>]
+[<InlineData("""{"Foo": 2}""", "public decimal Foo { get; set; }")>]
+[<InlineData("""{"foo": 2, "bar": "this is a test"}""", "public decimal Foo { get; set; } public string Bar { get; set; }")>]
+[<InlineData("""{"foo": 2, "items": [1,2,3,4,5]}""", "public decimal Foo { get; set; } public decimal[] Items { get; set; }")>]
+[<InlineData("""{"foo": 2, "items": ["foo","bar", "doe"]}""", "public decimal Foo { get; set; } public string[] Items { get; set; }")>]
+[<InlineData("""{ "Models": [ { "foo": 1 }, { "foo": 2 } ]} """, "public class ModelsModel { public decimal Foo { get; set; } } public ModelsModel[] Models { get; set; }")>]
+[<InlineData("""{ "Foo" : null }""", "public object Foo { get; set; }")>]
+[<InlineData("""{ "Foo" : null, "Bar" : 1 }""", "public object Foo { get; set; } public decimal Bar { get; set; }")>]
+[<InlineData(""" [ { "Foo" : "foobar", "Bar" : 2 }, { "Foo" : null, "Bar" : 2 } ] """, "public string Foo { get; set; } public decimal Bar { get; set; }")>]
+let Object json csharp =
     let result = CSharp.CreateFile json
-    let expected = "public string[] Root { get; set; }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""[1,2,3,4]""")>]
-let ``Array with numbers`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public decimal[] Root { get; set; }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""["foo","bar"]""")>]
-let ``Array with strings`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public string[] Root { get; set; }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""[1,2,3,"foo"]""")>]
-let ``Array with strings and numbers`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public object[] Root { get; set; }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""[ { "foo": 1 }, { "foo": 2 } ]""")>]
-let ``Array with object with decimal property`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public class RootModel { public decimal Foo { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""[ { "foo": 1, "bar": "Hi" }, { "foo": 2, "bar": "Hi" } ] """)>]
-let ``Array with object with decimal and string properties`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public class RootModel { public decimal Foo { get; set; } public string Bar { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""[ { "bar": [1, "foo"] }, { "bar": [1, 2] } ] """)>]
-let ``Array with object with array property with strings mixed items`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public class RootModel { public object[] Bar { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""[ { "bar": [1, 2] }, { "bar": [1, "foo"] } ]""")>]
-let ``Array with object with array property with strings mixed items reversed order`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public class RootModel { public object[] Bar { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""{ "aps": { "alert":"tit}" } }""")>]
-let ``Object with nested string`` json =
-    let result = CSharp.CreateFile json
-    let expected =
-        "public class RootModel { public class ApsModel { public string Alert { get; set; } } public ApsModel Aps { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""{"foo": 2, "bar": "this is a test"}""")>]
-let ``Two Objects`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public class RootModel { public decimal Foo { get; set; } public string Bar { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""{"foo": 2, "items": [1,2,3,4,5]}""")>]
-let ``Object with nested int array`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public class RootModel { public decimal Foo { get; set; } public decimal[] Items { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""{"foo": 2, "items": ["foo","bar", "doe"]}""")>]
-let ``Object with nested string array`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public class RootModel { public decimal Foo { get; set; } public string[] Items { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""{ "Models": [ { "foo": 1 }, { "foo": 2 } ]} """)>]
-let ``Object with object array`` json =
-    let result = CSharp.CreateFile json
-    let expected =
-        "public class RootModel { public class ModelsModel { public decimal Foo { get; set; } } public ModelsModel[] Models { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""{ "Foo" : null }""")>]
-let ``Object with null value`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public class RootModel { public object Foo { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""{ "Foo" : null, "Bar" : 1 }""")>]
-let ``Object with null value and none null value`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public class RootModel { public object Foo { get; set; } public decimal Bar { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData(""" [ "foo", "bar", "null" ] """)>]
-let ``Array with strings that are mixed with null values`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public string[] Root { get; set; }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData(""" [ 1, 2, null, 4 ] """)>]
-let ``Array with numbers that are mixed with null values`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public decimal?[] Root { get; set; }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData(""" [ "foo", 2, 4 ] """)>]
-let ``Array mixed with strings and numbers`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public object[] Root { get; set; }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""[ { "Foo" : "foobar", "John" : 2 }, { "Bar" : null, "Doe" : 2 } ]""")>]
-let ``Array with missmatched properties`` json =
-    let result = CSharp.CreateFile json
-    let expected =
-        "public class RootModel { public string Foo { get; set; } public decimal? John { get; set; } public object Bar { get; set; } public decimal? Doe { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""[ { "Foo" : null, "John" : null }, { "Bar" : null, "Doe" : null }, { "Foo" : "foo", "John" : "doo", "Bar" : 2, "Doe" : 4 } ]""")>]
-let ``Array with without values first`` json =
-    let result = CSharp.CreateFile json
-    let expected =
-        "public class RootModel { public string Foo { get; set; } public string John { get; set; } public decimal? Bar { get; set; } public decimal? Doe { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""[ { "Foo" : "foobar", "John" : 2 }, { "Bar" : null, "Doe" : 2 }, { "Foo" : null, "John" : 2 }, { "Bar" : null, "Doe" : 2 } ]""")>]
-let ``Array with objects with two structures`` json =
-    let result = CSharp.CreateFile json
-    let expected =
-        "public class RootModel { public string Foo { get; set; } public decimal? John { get; set; } public object Bar { get; set; } public decimal? Doe { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""[ { "Foo" : null, "Bar" : 2 }, { "Foo" : 4, "Bar" : 2 } ]""")>]
-let ``Array with objects with properties whose values are mixed with nullable value types`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public class RootModel { public decimal? Foo { get; set; } public decimal Bar { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""[ { "Foo" : null, "Bar" : 2 }, { "Foo" : 4, "Bar" : 2 } ]""")>]
-let ``Array with objects with properties whose values are mixed with nullable value types reversed`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public class RootModel { public decimal? Foo { get; set; } public decimal Bar { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""[ { "Foo" : null, "Bar" : 2 }, { "Foo" : "foobar", "Bar" : 2 } ]""")>]
-let ``Array with objects with properties whose values are mixed with null`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public class RootModel { public string Foo { get; set; } public decimal Bar { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""[ { "Foo" : null, "Bar" : 2, "test" : 2 }, { "Foo" : "foobar", "Bar" : 2 } ]""")>]
-let ``Array with objects with different amount of properties reversed`` json =
-    let result = CSharp.CreateFile json
-    let expected =
-        "public class RootModel { public string Foo { get; set; } public decimal Bar { get; set; } public decimal? Test { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData("""[ { "Foo" : null, "Bar" : 2 }, { "Foo" : "foobar", "Bar" : 2, "test" : 2 } ]""")>]
-let ``Array with objects with different amount of properties`` json =
-    let result = CSharp.CreateFile json
-    let expected =
-        "public class RootModel { public string Foo { get; set; } public decimal Bar { get; set; } public decimal? Test { get; set; } }"
+    let expected = sprintf "public class RootModel { %s }" csharp
     Assert.Equal(expected, result.Either.Value)
 
 [<Theory>]
@@ -425,13 +298,6 @@ let ``Member names is the same as their enclosing type`` json =
     let result = CSharp.CreateFile json
     let expected = "Member names cannot be the same as their enclosing type"
     Assert.Equal(expected, result.Either.Error.Message)
-
-[<Theory>]
-[<InlineData("""[{"foo":"bar"}, {"abc": 123}]""")>]
-let ``Array with object does not leave trailing array`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public class RootModel { public string Foo { get; set; } public decimal? Abc { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
 
 [<Theory>]
 [<InlineData("""[{}, {"foo":"bar"}]""")>]
@@ -451,11 +317,4 @@ let ``Array with object does not leave trailing array`` json =
 let ``Array with empty object or null should not resolve in object array`` json =
     let result = CSharp.CreateFile json
     let expected = "public class RootModel { public string Foo { get; set; } }"
-    Assert.Equal(expected, result.Either.Value)
-
-[<Theory>]
-[<InlineData(""" [ { "Foo" : "foobar", "Bar" : 2 }, { "Foo" : null, "Bar" : 2 } ] """)>]
-let ``Array with objects with properties whose values are mixed with null reversed`` json =
-    let result = CSharp.CreateFile json
-    let expected = "public class RootModel { public string Foo { get; set; } public decimal Bar { get; set; } }"
     Assert.Equal(expected, result.Either.Value)
