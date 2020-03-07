53ef9ded7f1635f67578451c99f7e981e41925e1
Robert Andersson <robert.andersson.swe@gmail.com>
2020-03-07 19:44:42 +0100

    Rename property to be pascal cased

--- src/libs/Common/CaseInsensitiveString.fs
+++ src/libs/Common/CaseInsensitiveString.fs
@@ -6,21 +6,21 @@ type CIString =
     | CI of string
     override this.Equals x =
         match x with
-        | :? CIString as s -> StringComparer.OrdinalIgnoreCase.Equals(this.string, s.string) 
-        | :? string as s -> StringComparer.OrdinalIgnoreCase.Equals(this.string, s) 
-        | x when x = null && this.string = null -> true
+        | :? CIString as s -> StringComparer.OrdinalIgnoreCase.Equals(this.String, s.String) 
+        | :? string as s -> StringComparer.OrdinalIgnoreCase.Equals(this.String, s) 
+        | x when x = null && this.String = null -> true
         | _ -> false
-    member this.string =
+    member this.String =
         match this with
         | CI x -> x
     override this.GetHashCode() =
-        StringComparer.OrdinalIgnoreCase.GetHashCode(this.string)
+        StringComparer.OrdinalIgnoreCase.GetHashCode(this.String)
         
     interface System.IComparable<CIString> with
-            member this.CompareTo x = StringComparer.OrdinalIgnoreCase.Compare(this.string, x.string)
+            member this.CompareTo x = StringComparer.OrdinalIgnoreCase.Compare(this.String, x.String)
     interface System.IComparable with
             member this.CompareTo x =
                 match x with 
-                | :? CIString as s -> StringComparer.OrdinalIgnoreCase.Compare(this.string, s.string) 
-                | :? string as s -> StringComparer.OrdinalIgnoreCase.Compare(this.string, s) 
+                | :? CIString as s -> StringComparer.OrdinalIgnoreCase.Compare(this.String, s.String) 
+                | :? string as s -> StringComparer.OrdinalIgnoreCase.Compare(this.String, s) 
                 | _ -> -1
