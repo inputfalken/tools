a38f242841f79cc4b3094e51c62791222489682c
Robert Andersson <robert.andersson.swe@gmail.com>
2020-03-16 22:03:52 +0100

    Fix powershell output

--- src/apps/PowerShellModule/CSharpFactory.fs
+++ src/apps/PowerShellModule/CSharpFactory.fs
@@ -26,30 +26,24 @@ type CSharpFactory() =
     [<Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true, HelpMessage = "The JSON.")>]
     member val InputObject: string = "" with get, set
 
-
     [<ValidateSet(CasingConstants.Camel, CasingConstants.Pascal, CasingConstants.None)>]
     [<Parameter(Mandatory = false, Position = 1, HelpMessage = "Sets the casing rules for class names.")>]
     member val ClassCasing: string = CasingConstants.Pascal with get, set
-
 
     [<ValidateSet(CasingConstants.Camel, CasingConstants.Pascal, CasingConstants.None)>]
     [<Parameter(Mandatory = false, Position = 2, HelpMessage = "Sets the casing rules for property names.")>]
     member val PropertyCasing: string = CasingConstants.Pascal with get, set
 
-
     [<ValidateNotNullOrEmpty>]
     [<Parameter(Mandatory = false, Position = 3, HelpMessage = "Sets the name of the namespace.")>]
     member val NameSpace: string = "" with get, set
-
 
     [<ValidateNotNullOrEmpty>]
     [<Parameter(Mandatory = false, Position = 4, HelpMessage = "Sets the name of the root object.")>]
     member val ObjectName: string = "Root" with get, set
 
-
     [<Parameter(Mandatory = false, Position = 5, HelpMessage = "Sets a prefix for the class names.")>]
     member val Prefix: string = "" with get, set
-
 
     [<Parameter(Mandatory = false, Position = 6, HelpMessage = "Suffix a prefix for the class names.")>]
     member val Suffix: string = "" with get, set
@@ -63,5 +57,8 @@ type CSharpFactory() =
             CSharpSettings
                 (PropertyCasing = x.PropertyCasing, ClassCasing = x.ClassCasing, NameSpace = x.NameSpace,
                  ClassPrefix = x.Prefix, ClassSuffix = x.Suffix, RootObjectName = x.ObjectName)
+                
         // TODO add proper support like https://github.com/PowerShell/PowerShell/blob/master/src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/ConvertFromJsonCommand.cs
-        Factory.CSharp(String.Join(Environment.NewLine, x.Buffer), settings) |> x.WriteObject
+        Factory.CSharp(String.Join(Environment.NewLine, x.Buffer), settings)
+               .Match((fun (x: String) -> x), (fun (x: exn) -> raise x))
+        |> x.WriteObject
