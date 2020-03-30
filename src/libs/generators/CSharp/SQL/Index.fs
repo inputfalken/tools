d6c98c9d92c9a287dfa624265467f415ded63377
Robert Andersson <robert.andersson.swe@gmail.com>
2020-03-30 20:53:16 +0200

    Add tests for basetypes

--- src/libs/generators/CSharp/SQL/Index.fs
+++ src/libs/generators/CSharp/SQL/Index.fs
@@ -7,12 +7,16 @@ open System.Linq
 open Microsoft.CodeAnalysis.CSharp
 open Microsoft.CodeAnalysis.CSharp.Syntax
 
-let private mapBaseTypes (``type``: PredefinedTypeSyntax) =
-    match ``type``.Keyword.RawKind with
-    | 8309 -> "int"
-    | 8304 -> "bit"
-    | 8316 -> "nvarchar(max)"
+// Source: https://github.com/dotnet/roslyn/blob/master/src/Compilers/CSharp/Portable/Syntax/SyntaxKind.cs
+let private mapBaseTypes (property:  PropertyDeclarationSyntax): String =
+    match property.Type.ToString()  with
+    | "int" -> "int"
+    | "bool" -> "bit"
+    | "string" -> "nvarchar(max)"
+    | "GUID" -> "uniqueidentifier"
+    | "DateTime" -> "datetime"
     | _ -> raise (NotImplementedException("TODO"))
+    |> sprintf "@%s %s" property.Identifier.Text
 
 let foo (procedureName: string) (cSharp: string) =
     let res = CSharpSyntaxTree.ParseText(cSharp) |> CSharpExtensions.GetCompilationUnitRoot
@@ -25,12 +29,10 @@ let foo (procedureName: string) (cSharp: string) =
     let properties =
         ``class``.Members
         |> Enumerable.OfType<PropertyDeclarationSyntax>
-        |> Seq.map (fun x -> x.Type)
-        |> Seq.cast<PredefinedTypeSyntax>
         |> Seq.map mapBaseTypes
         |> StringJoin.joinStringsWithCommaSpaceSeparation
 
     let storedProcedure =
-        sprintf "CREATE OR ALTER PROCEDURE %s (%s) AS \nBEGIN\nEND" procedureName properties
+        sprintf "CREATE OR ALTER PROCEDURE %s (%s) AS\nBEGIN\n\nEND" procedureName properties
 
     storedProcedure
