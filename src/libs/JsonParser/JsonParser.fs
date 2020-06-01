cbaaf24d9a89dd1422d21005501248fbd014bb34
Robert Andersson <robert.andersson@offerta.se>
2020-06-01 10:19:48 +0200

    Simplify pattern matching abit

--- src/libs/JsonParser/JsonParser.fs
+++ src/libs/JsonParser/JsonParser.fs
@@ -1,35 +1,41 @@
 namespace JsonParser
 
-open System
 open System.Globalization
 open FSharp.Data
 open FSharp.Data.Runtime
 
 module public Json =
     let private culture = CultureInfo.InvariantCulture
-    let private emptyArray = Array.empty<String>
+
     let rec private map (value: JsonValue) =
-        JsonConversions.AsInteger culture value
-        |> Option.map Value.Int
-        |> Option.orElseWith (fun () -> JsonConversions.AsDecimal culture value |> Option.map Value.Decimal)
-        |> Option.orElseWith (fun () -> JsonConversions.AsFloat emptyArray true culture value |> Option.map Decimal |> Option.map Value.Decimal)
-        |> Option.orElseWith (fun () -> JsonConversions.AsGuid value |> Option.map Value.Guid)
-        |> Option.orElseWith (fun () -> JsonConversions.AsDateTime culture value |> Option.map Value.DateTime)
-        |> Option.orElseWith (fun () -> JsonConversions.AsBoolean value |> Option.map Value.Boolean)
-        |> Option.defaultWith (fun () ->
-            match value with
-            | JsonValue.String x -> Value.String x
-            | JsonValue.Null -> Value.Null
-            | JsonValue.Array x ->
-                x
-                |> Array.map map
-                |> Value.Array
-            | JsonValue.Record x ->
-                x
-                |> Array.map (fun (x, y) ->
-                    { Key = x
-                      Value = map y })
-                |> Value.Object)
+        match value with
+        | JsonValue.Record x ->
+            x
+            |> Array.map (fun (x, y) ->
+                { Key = x
+                  Value = map y })
+            |> Value.Object
+        | JsonValue.Array x ->
+            x
+            |> Array.map map
+            |> Value.Array
+        | x ->
+            
+            JsonConversions.AsInteger culture x
+            |> Option.map Value.Int
+            |> Option.orElseWith (fun () -> JsonConversions.AsGuid x |> Option.map Value.Guid)
+            |> Option.orElseWith (fun () -> JsonConversions.AsDateTime culture x |> Option.map Value.DateTime)
+            |> Option.defaultWith (fun () ->
+                match x with
+                | JsonValue.String x -> Value.String x
+                | JsonValue.Boolean x -> Value.Boolean x
+                | JsonValue.Null -> Value.Null
+                | JsonValue.Number x -> Value.Decimal x
+                | JsonValue.Record _
+                | JsonValue.Array _
+                | JsonValue.Float _
+                | JsonValue.Number _ -> failwith "Should never happen.")
+
 
     let public parse input =
         input
