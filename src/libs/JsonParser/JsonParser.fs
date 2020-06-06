028d6371196626211e93bc6957f98768e557c928
Robert Andersson <robert.andersson@offerta.se>
2020-06-06 14:27:21 +0200

    Try do most of the pattern matching before falling back on conversion logic

--- src/libs/JsonParser/JsonParser.fs
+++ src/libs/JsonParser/JsonParser.fs
@@ -3,6 +3,23 @@ namespace JsonParser
 open System.Globalization
 open FSharp.Data
 open FSharp.Data.Runtime
+open System
+
+type public Value =
+    | Int of int
+    | Double of double
+    | Decimal of decimal
+    | String of string
+    | DateTime of DateTime
+    | Boolean of Boolean
+    | Array of Value []
+    | Guid of Guid
+    | Null
+    | Object of Record []
+
+and public Record =
+    { Key: String
+      Value: Value }
 
 module public Json =
     let private culture = CultureInfo.InvariantCulture
@@ -20,21 +37,23 @@ module public Json =
             |> Array.map map
             |> Value.Array
         | x ->
-            
-            JsonConversions.AsInteger culture x
-            |> Option.map Value.Int
-            |> Option.orElseWith (fun () -> JsonConversions.AsGuid x |> Option.map Value.Guid)
-            |> Option.orElseWith (fun () -> JsonConversions.AsDateTime culture x |> Option.map Value.DateTime)
-            |> Option.defaultWith (fun () ->
-                match x with
-                | JsonValue.String x -> Value.String x
-                | JsonValue.Boolean x -> Value.Boolean x
-                | JsonValue.Null -> Value.Null
-                | JsonValue.Number x -> Value.Decimal x
-                | JsonValue.Record _
-                | JsonValue.Array _
-                | JsonValue.Float _
-                | JsonValue.Number _ -> failwith "Should never happen.")
+            match x with
+            | JsonValue.Boolean x -> Value.Boolean x
+            | JsonValue.Null -> Value.Null
+            | x ->
+                let option =
+                    JsonConversions.AsInteger culture x
+                    |> Option.map Value.Int
+                    |> Option.orElseWith (fun () -> JsonConversions.AsGuid x |> Option.map Value.Guid)
+                    |> Option.orElseWith
+                        (fun () -> JsonConversions.AsDateTime culture x |> Option.map Value.DateTime)
+                match option with
+                | Some x -> x
+                | _ ->
+                    match x with
+                    | JsonValue.String x -> Value.String x
+                    | JsonValue.Number x -> Value.Decimal x
+                    | _ -> failwith "Should never happen."
 
 
     let public parse input =
