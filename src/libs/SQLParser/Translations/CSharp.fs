6a523ae0583eb6ec5e1b285bf8a18a5a5063752e
Robert Andersson <inputfalken@gmail.com>
2021-05-14 13:29:08 +0200

    WIP add support for setters

--- src/libs/SQLParser/Translations/CSharp.fs
+++ src/libs/SQLParser/Translations/CSharp.fs
@@ -23,20 +23,20 @@ let toCSharp sql =
             (fun x ->
                 let translation =
                     match x.DataType with
-                    | Char _ -> BaseType.String String.Empty
-                    | Date -> BaseType.DateTime DateTime.MinValue
+                    | Char _ -> BaseType.String String.Empty option.None
+                    | Date -> BaseType.DateTime DateTime.MinValue option.None
                     | Bit -> BaseType.Boolean false
-                    | DateTime -> BaseType.DateTime DateTime.MinValue
-                    | DateTime2 _ -> BaseType.DateTime DateTime.MinValue
-                    | DateTimeOffset _ -> BaseType.DateTime DateTime.MinValue
-                    | Int _ -> BaseType.Integer 0
-                    | Decimal _ -> BaseType.Decimal 0m
-                    | NChar _ -> BaseType.String String.Empty
-                    | Nvarchar _ -> BaseType.String String.Empty
-                    | SmallDateTime -> BaseType.DateTime DateTime.MinValue
-                    | Time _ -> BaseType.DateTime DateTime.MinValue
-                    | UniqueIdentifier -> BaseType.Guid System.Guid.Empty
-                    | Varchar _ -> BaseType.String String.Empty
+                    | DateTime -> BaseType.DateTime DateTime.MinValue  option.None
+                    | DateTime2 _ -> BaseType.DateTime DateTime.MinValue option.None
+                    | DateTimeOffset _ -> BaseType.DateTime DateTime.MinValue option.None
+                    | Int _ -> BaseType.Integer 0 option.None
+                    | Decimal _ -> BaseType.Decimal 0m option.None
+                    | NChar _ -> BaseType.String String.Empty option.None
+                    | Nvarchar _ -> BaseType.String String.Empty option.None
+                    | SmallDateTime -> BaseType.DateTime DateTime.MinValue option.None
+                    | Time _ -> BaseType.DateTime DateTime.MinValue option.None
+                    | UniqueIdentifier -> BaseType.Guid System.Guid.Empty option.None
+                    | Varchar _ -> BaseType.String String.Empty option.None
                     |> CSType.BaseType
 
                 { Name = x.Name; Type = Option.Some translation })
