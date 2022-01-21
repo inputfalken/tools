e8b8f4a5e1ad7c267657f1d6eaba992c5a63ee4d
Robert Andersson <inputfalken@gmail.com>
2022-01-21 15:03:34 +0100

    Repair tests

--- test/CSharpGeneratorTests/GenerateProcedureTests.fs
+++ test/CSharpGeneratorTests/GenerateProcedureTests.fs
@@ -7,9 +7,9 @@ open Xunit
 [<Theory>]
 [<InlineData("public class Person {}", "CREATE OR ALTER PROCEDURE Person () AS\nBEGIN\n\nEND")>]
 [<InlineData("public class Person { public int Id { get; set; } }",
-             "CREATE OR ALTER PROCEDURE Person (@Id int) AS\nBEGIN\n\nEND")>]
+             "CREATE OR ALTER PROCEDURE Person (@Id INT) AS\nBEGIN\n\nEND")>]
 [<InlineData("public class Person { public int Id { get; set; } public string FirstName { get; set; } }",
-             "CREATE OR ALTER PROCEDURE Person (@Id int, @FirstName nvarchar(max)) AS\nBEGIN\n\nEND")>]
+             "CREATE OR ALTER PROCEDURE Person (@Id INT, @FirstName NVARCHAR(MAX)) AS\nBEGIN\n\nEND")>]
 let Class input expected =
     let result = generateStoredProcedureFromCSharp input { GenerationType = GenerationType.None }
     Assert.Equal(expected, result, false, true, true)
--- test/SqlGeneratorTests/SqlDataTypeToStringTests.fs
+++ test/SqlGeneratorTests/SqlDataTypeToStringTests.fs
@@ -1,24 +1,23 @@
 module SqlGeneratorTests.DataTypeTests
 
 open Xunit
-open Sql.Generator
 open Languages.SQL
 
 
 [<Fact>]
 let Float () =
     let res = Float.ToString()
-    Assert.Equal("float", res)
+    Assert.Equal("FLOAT", res)
 
 [<Fact>]
 let Int () =
     let res = Int.ToString()
-    Assert.Equal("int", res)
+    Assert.Equal("INT", res)
 
 [<Fact>]
 let Bool () =
     let res = Bit.ToString()
-    Assert.Equal("bit", res)
+    Assert.Equal("BIT", res)
 
 [<Theory>]
 [<InlineData(10)>]
@@ -26,21 +25,26 @@ let Bool () =
 [<InlineData(200)>]
 let String length =
     let res = (Nvarchar <| Number length).ToString()
-    let expected = sprintf "nvarchar(%d)" length
+    let expected = sprintf "NVARCHAR(%d)" length
     Assert.Equal(expected, res)
 
 [<Fact>]
 let StringMax () =
     let res = (Nvarchar <| Max).ToString()
-    let expected = sprintf "nvarchar(max)"
+    let expected = sprintf "NVARCHAR(MAX)"
     Assert.Equal(expected, res)
 
 [<Fact>]
 let Guid () =
     let res = UniqueIdentifier.ToString()
-    Assert.Equal("uniqueidentifier", res)
+    Assert.Equal("UNIQUEIDENTIFIER", res)
 
 [<Fact>]
 let DateTime () =
     let res = DateTime.ToString()
-    Assert.Equal("datetime", res)
+    Assert.Equal("DATETIME", res)
+    
+[<Fact>]
+let Date () =
+    let res = Date.ToString()
+    Assert.Equal("DATE", res)
